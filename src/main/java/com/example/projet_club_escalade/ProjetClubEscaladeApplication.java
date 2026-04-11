776943cee5b237b417c49e7b2c40a57ea87271f2
Mohammed Toujad <mohammedtd@Mohammeds-MacBook-Air.local>
2026-04-11 22:24:02 +0200

    Initialisation du projet

---
+++ src/main/java/com/example/projet_club_escalade/ProjetClubEscaladeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_club_escalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetClubEscaladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetClubEscaladeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet_club_escalade/ProjetClubEscaladeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_club_escalade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetClubEscaladeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
