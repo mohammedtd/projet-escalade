3828f22e4466a5cd56bdaa15bc075dfaf9d2a61b
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-15 17:59:03 +0200

    ajouter une methode et une QUERY

--- src/main/java/repository/CategorieRepository.java
+++ src/main/java/repository/CategorieRepository.java
@@ -1,9 +1,16 @@
 package repository;
 
+import model.Sortie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Categorie;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface CategorieRepository extends JpaRepository<Categorie, Long> {
+  @Query("SELECT c FROM Categorie c LEFT JOIN FETCH c.sorties WHERE c.categorieID = :id")
+  Categorie findByIdWithSorties(@Param("id") long id);
 }
