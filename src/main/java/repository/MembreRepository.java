aca17790cd5531458e18528c79d7a12457124083
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-15 17:25:11 +0200

    ajout d'une méthode et d'une query a mon jpa

--- src/main/java/repository/MembreRepository.java
+++ src/main/java/repository/MembreRepository.java
@@ -3,7 +3,12 @@ package repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Membre;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MembreRepository extends JpaRepository<Membre, Integer>{
+public interface MembreRepository extends JpaRepository<Membre, Long>{
+
+  @Query("SELECT m FROM Membre m LEFT JOIN FETCH m.sorties WHERE m.membreID = :id")
+  Membre findByIdWithSorties(@Param("id") long id);
 
 }
