c588d137b2d9d591a776f96074e0926b12255bcb
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-16 16:53:55 +0200

    changer le nom de la classe Main

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -3,13 +3,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "controller", "service", "dao" })
+@SpringBootApplication(scanBasePackages = {"web", "service", "dao" })
 @EntityScan("model")
 @EnableJpaRepositories("repository")
-public class ProjetClubEscaladeApplication {
+public class AppMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjetClubEscaladeApplication.class, args);
+        SpringApplication.run(AppMain.class, args);
     }
 
 }
