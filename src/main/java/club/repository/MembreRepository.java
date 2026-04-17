7a07b69201c6c984b0a1b2a836ba82fc5a5ad59a
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-17 14:41:28 +0200

    ajouter methode findByEmail() dans MembreRepository.java

--- src/main/java/club/repository/MembreRepository.java
+++ src/main/java/club/repository/MembreRepository.java
@@ -8,11 +8,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional
 public interface MembreRepository extends JpaRepository<Membre, Long>{
 
   @Query("SELECT m FROM Membre m LEFT JOIN FETCH m.sorties WHERE m.membreID = :id")
   Membre findByIdWithSorties(@Param("id") long id);
+
+  Optional<Membre> findByEmail(String email);
 
 }
