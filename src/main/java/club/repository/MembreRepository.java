132e63ad495b1e1f8d53e8d9f929b456d37e1f18
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-17 13:46:09 +0200

    ajouter des  annotation  pour mon MembreRepository.java

--- src/main/java/club/repository/MembreRepository.java
+++ src/main/java/club/repository/MembreRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import club.model.Membre;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface MembreRepository extends JpaRepository<Membre, Long>{
 
   @Query("SELECT m FROM Membre m LEFT JOIN FETCH m.sorties WHERE m.membreID = :id")
