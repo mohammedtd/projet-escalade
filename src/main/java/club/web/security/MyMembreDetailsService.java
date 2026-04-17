fc120d162a5707a1251102b23e5c1d7db9a4b5de
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-17 14:40:53 +0200

    ajouter classe MyMembreDetailsService.java implementent UserDetailService . cette classe permet de montrer comment touver le membre dans mon app

---
+++ src/main/java/club/web/security/MyMembreDetailsService.java
@@ -0,0 +1,30 @@
+package club.web.security;
+
+import club.model.Membre;
+import club.repository.MembreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyMembreDetailsService implements UserDetailsService {
+
+  @Autowired
+  private MembreRepository memberRepository;
+
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    Membre membre = memberRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Membre non trouvé"));
+    return User.withUsername(membre.getEmail())
+        .password(membre.getMotDePasse())
+        .authorities("ROLE_MEMBER")
+        .disabled(false)
+        .accountExpired(false)
+        .accountLocked(false)
+        .credentialsExpired(false)
+        .build();
+  }
+}
