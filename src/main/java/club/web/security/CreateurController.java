fc112d95a794c14dd6f5b10c97005f2900549821
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-18 21:46:44 +0200

    add CreateurController.java

---
+++ src/main/java/club/web/security/CreateurController.java
@@ -0,0 +1,20 @@
+package club.web.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import club.service.MembreService;
+
+@Controller
+public class CreateurController {
+  private final MembreService membreService;
+  public CreateurController(MembreService membreService) {
+    this.membreService = membreService;
+  }
+
+  @GetMapping("/createur")
+  public String listCreateurs(Model model) {
+    model.addAttribute("membres", membreService.getAllMembres());
+    return "createur";
+  }
+}
