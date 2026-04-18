8eaed45f1262fb1ff6d8f0677b3dd65a2e4c1fd7
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-18 19:59:14 +0200

    alter AccueilController.java utiliser services au lieu de dao et ajouter des methodes pour recuperer des données de ma BdD

--- src/main/java/club/web/AccueilController.java
+++ src/main/java/club/web/AccueilController.java
@@ -1,6 +1,9 @@
 package club.web;
 
 import club.dao.CategorieDAO;
+import club.service.CategorieService;
+import club.service.MembreService;
+import club.service.SortieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +12,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccueilController {
 
-  private final CategorieDAO categorieDAO;
+  private final CategorieService categorieService;
+  private final SortieService sortieService;
+  private final MembreService membreService;
+  public AccueilController(CategorieService categorieService,
+                           SortieService sortieService,
+                           MembreService membreService) {
 
-  public AccueilController(CategorieDAO categorieDAO) {
-    this.categorieDAO = categorieDAO;
+    this.categorieService = categorieService;
+    this.sortieService = sortieService;
+    this.membreService = membreService;
   }
 
   @GetMapping("/")
-  public String home(Model model) {
+  public String accueil() {
     return "accueil";
   }
 
   @GetMapping("/home")
-  public String visit(Model model) {
+  public String home(Model model) {
+    model.addAttribute("categories", categorieService.getAllCategories());
+    model.addAttribute("sorties", sortieService.getAllSorties());
+    model.addAttribute("membres", membreService.getAllMembres());
     return "home";
   }
+
+
 
 
 }
