201ef59a6695223578396c54b1daff0f460b80f2
douaa <douaa.benhila@etu.univ-amu.fr>
2026-04-18 21:32:08 +0200

    alter SortieController.java pour afficher des pages et les données

--- src/main/java/club/web/SortieController.java
+++ src/main/java/club/web/SortieController.java
@@ -1,90 +1,29 @@
 package club.web;
 
-import java.time.LocalDate;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import club.model.Sortie;
-import club.service.CategorieService;
 import club.service.SortieService;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
-@RequestMapping("/sorties")
 public class SortieController {
     private final SortieService sortieService;
-    private final CategorieService categorieService;
-
-
-    public SortieController(SortieService sortieService, CategorieService categorieService) {
+    public SortieController(SortieService sortieService) {
         this.sortieService = sortieService;
-        this.categorieService = categorieService;
-
     }
 
-    /*@GetMapping
-    public List<Sortie> getAll() {
-        return sortieService.getAllSorties();
+    @GetMapping("/sorties")
+    public String listSorties(Model model) {
+        model.addAttribute("sorties", sortieService.getAllSorties());
+        return "sortie";
     }
-
-    @GetMapping("/{id}")
-    public Sortie getById(@PathVariable Integer id) {
-        return sortieService.getSortie(id);
-    }
-
-    @PostMapping
-    public Sortie save(@RequestBody Sortie sortie) {
-        return sortieService.saveSortie(sortie);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
-        sortieService.deleteSortie(id);
-    }
-
-    @GetMapping("/search")
-    public List<Sortie> search(
-            @RequestParam(required = false) String nom,
-            @RequestParam(required = false) Integer categorieId,
-            @RequestParam(required = false) Long createurId,
-            @RequestParam(required = false) LocalDate dateSortie) {
-        return sortieService.search(nom, categorieId, createurId, dateSortie);
-    }*/
-
-    @GetMapping("/{id}")
-    public String getById(@PathVariable Integer id, Model model) {
+    @GetMapping("/sorties/{id}")
+    public String detailSortie(@PathVariable Long id, Model model) {
         Sortie sortie = sortieService.getSortie(id);
         model.addAttribute("sortie", sortie);
         return "detailSortie";
     }
-
-    @GetMapping("/search")
-    public String search(
-        @RequestParam(required = false) String nom,
-        @RequestParam(required = false) Integer categorieId,
-        @RequestParam(required = false) Long createurId,
-        @RequestParam(required = false) LocalDate dateSortie,
-        Model model) {
-
-
-        if (nom != null && nom.trim().isEmpty()) {
-            nom = null;
-        }
-
-        model.addAttribute("sorties", sortieService.search(nom, categorieId, createurId, dateSortie));
-        model.addAttribute("categories", categorieService.getAllCategories());
-
-        model.addAttribute("nom", nom);
-        model.addAttribute("categorieId", categorieId);
-        model.addAttribute("createurId", createurId);
-        model.addAttribute("dateSortie", dateSortie);
-
-        return "resultatRecherche";
-    }
 }
-
